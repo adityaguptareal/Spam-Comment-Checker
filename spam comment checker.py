c47a428de7ab15455eab03f2e13b6071225df2d9
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2022-11-02 17:48:20 +0530

    Spam Comment or text checker
    Basic comment or text checker using python

---
+++ spam comment checker.py
@@ -0,0 +1,13 @@
+user_comment=input("Enter the text here: ")
+if ("make a lots of money" in user_comment):
+    print("The Comment is spam")
+elif("Buy now" in user_comment):
+    print("The comment is spam")
+elif("Buy now" in user_comment):
+    print("The comment is spam")
+elif("Click the link" in user_comment):
+    print("The comment is spam")
+elif("watch the video" in user_comment):
+    print("The comment is spam")
+else:
+    print("The comment is geniune")
